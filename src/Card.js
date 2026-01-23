60a346d53aa703cfa1f4ef9424678524ac38ff96
Deyane <deyane.sheikh@insa-lyon.fr>
2026-01-23 17:41:32 +0100

    MAJ des Cards et Deck

---
+++ src/Card.js
@@ -0,0 +1,11 @@
+class Deck {
+  constructor() {
+    this.cards = [];
+  }
+  shuffle() {
+    // Shuffle logic ici
+  }
+  draw() {
+    return this.cards.pop();
+  }
+}
---
+++ src/Deck.js
@@ -0,0 +1,11 @@
+
+Cartes = Array();
+
+for (i=0 ; i<13; i++) {
+    Cartes.push(i);
+}
+
+Action = Array();
+Action.push("Freeze");
+Action.push("Flip Three");
+Action.push("Seconde Chance");
