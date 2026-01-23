a90b8c7dfdf441a4038f0bef1a5b0dd921a372b7
Deyane <deyane.sheikh@insa-lyon.fr>
2026-01-23 17:26:34 +0100

    Initialisation du projet'

---
+++ COURS/cours.js
@@ -0,0 +1,14 @@
+function somme(a,b){
+return a+b
+}
+
+console.log(somme(3,5))
+
+console.log("suite ... \n nouvelle ligne\n")
+
+console.log("Appolo" + 5)
+
+console.log("suite ... \n nouvelle ligne\n")
+
+console.log(null + "ify")
+
---
+++ COURS/fonction.js
@@ -0,0 +1,20 @@
+    function moins(a, b, c) {
+      return (a - b);
+    }
+
+    console.log(moins(3, 2));
+    console.log(moins(3, 2, 4, 8));
+    console.log(moins(2));
+
+    console.log("fin du calcul de la soustraction\n");
+    
+    console.log("Résultat attendu : " + 1); 
+
+    mesAmis = ["bob", "raoul", "louis"]
+    for (i = 0; i < mesAmis.length; i++) {
+      console.log("->", mesAmis[i]+ "\n");
+    }
+    tesAmis = new Array();
+    tesAmis.push(1);
+    tesAmis.push('leon');
+
